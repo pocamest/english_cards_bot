1120423f9a8be3063f0a0d2e8fed6990f07eac07
pocamest <pocamest@yandex.ru>
2025-01-27 14:00:48 +0300

    Updated get_translation_optionals

--- services/services.py
+++ services/services.py
@@ -2,11 +2,11 @@ import random
 
 
 def get_translation_optionals(
-    cards: dict[str, str], word: str
+    word_translations: dict[str, str], word: str
 ) -> tuple[str, list[str]]:
-    translation = cards[word]
+    translation = word_translations[word]
     other_translation = [
-        value for key, value in cards.items()
+        value for key, value in word_translations.items()
         if key != word and value != translation
     ]
     optional_translation = random.sample(
