19eedac6ddab617bf92e4ac1550944adc7ccbdda
pocamest <pocamest@yandex.ru>
2025-01-30 01:27:48 +0300

    Added IsCorrectWord and IsCorrectTranslation filters

--- filters/filters.py
+++ filters/filters.py
@@ -1,5 +1,6 @@
 from aiogram.filters import BaseFilter
 from aiogram.types import Message, CallbackQuery
+import re
 
 
 class IsPage(BaseFilter):
@@ -24,5 +25,23 @@ class IsDeleteWord(BaseFilter):
         if name == 'del':
             return {'word': word}
 
+        else:
+            return False
+
+
+class IsCorrectWord(BaseFilter):
+    async def __call__(self, message: Message) -> dict[str, str] | bool:
+        word = message.text.strip()
+        if word and bool(re.fullmatch(r'^[А-Яа-яёЁ- ]+$', word)):
+            return {'word': word.lower()}
+        else:
+            return False
+
+
+class IsCorrectTranslation(BaseFilter):
+    async def __call__(self, message: Message) -> dict[str, str] | bool:
+        translation = message.text.strip()
+        if translation and bool(re.fullmatch(r"^[A-Za-z'- ]+$", translation)):
+            return {'translation': translation.lower()}
         else:
             return False
