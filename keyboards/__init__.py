af4b387832f0871349177949beae0f575770ece7
pocamest <pocamest@yandex.ru>
2025-01-31 21:24:15 +0300

    Added main_menu

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,3 +1,4 @@
 from .generic_kb import create_generic_keyboard
 from .training_kb import create_training_keyboard
 from .cards_kb import create_cards_keyboard
+from .main_menu import set_main_menu
--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
 from database import DataBase, add_default_words
 from config_data import load_config, Config
 from handlers import router
 from middlewares import DataBaseMiddleware
-from aiogram.fsm.storage.memory import MemoryStorage
+from keyboards import set_main_menu
 
 logger = logging.getLogger(__name__)
 
@@ -20,7 +21,19 @@ DEFAULT_WORDS = [
     {"word": "оранжевый", "translation": "orange"},
     {"word": "фиолетовый", "translation": "purple"},
     {"word": "розовый", "translation": "pink"},
-    {"word": "серый", "translation": "gray"}]
+    {"word": "серый", "translation": "gray"},
+    {"word": "коричневый", "translation": "brown"},
+    {"word": "бирюзовый", "translation": "turquoise"},
+    {"word": "голубой", "translation": "light blue"},
+    {"word": "золотистый", "translation": "golden"},
+    {"word": "лимонный", "translation": "lemon"},
+    {"word": "малиновый", "translation": "raspberry"},
+    {"word": "оливковый", "translation": "olive"},
+    {"word": "салатовый", "translation": "salad green"},
+    {"word": "сиреневый", "translation": "lilac"},
+    {"word": "темно-синий", "translation": "navy blue"},
+    {"word": "бледно-розовый", "translation": "pale pink"}
+]
 
 
 async def main():
@@ -41,6 +54,8 @@ async def main():
 
     bot = Bot(token=config.tg_bot.token)
     dp = Dispatcher(storage=storage)
+
+    await set_main_menu(bot)
 
     session_factory = db.session_factory
     dp.update.middleware(DataBaseMiddleware(session_factory))
